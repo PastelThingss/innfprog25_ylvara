4d657453ec87544ca24473c2a48f51f723d9f3ee
PastelThingss <143091459+PastelThingss@users.noreply.github.com>
2025-11-27 20:01:25 +0100

    started to add some code
    started to build my base structure of the code

---
+++ eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/register.js
@@ -0,0 +1,13 @@
+//this file shall include the data structure of the workers(ansatte) at a university. one worker shall have a last name and a first name. which office they are at(sitting at), also which work
+//postions they have and the name of the work postion they have (that spesific worker). their work mail (mail adress) and for last it shall also include which courses they have or have the responsibility for.
+//the work postions they can have are: Professor, Dekan, Janitor, Principal. (Norwegian: Rektor, Dekan, Vaktmester og Lektor). The notes to myself about the task and so has been moved to a seperate file as to not clog this one up.
+const workers = [
+    {
+        firstname: "",
+        lastname: "",
+        office: "",
+        position: "",
+        mail: "",
+        courses: "",
+    }
+]
