83e6640595cdac53b1448c795e0e42aead8eab8c
PastelThingss <143091459+PastelThingss@users.noreply.github.com>
2025-12-03 00:44:15 +0100

    added a css style sheet and some test code in register js
    my regiuster file might work or be corrupt becuase of a pc crash in middle of a upload we shall see of this works

--- eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/register.js
+++ eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/register.js
@@ -1,13 +1,29 @@
+//I prefer writing code and comments in English even dho the task we have got is in Norwegian. I might use some of the names or words you used in the videos as names for some code fucntions and so on.
+// Since some of them will be weird to translate to english and the site isteself are gonna show its content in Norwegian.
+
 //this file shall include the data structure of the workers(ansatte) at a university. one worker shall have a last name and a first name. which office they are at(sitting at), also which work
 //postions they have and the name of the work postion they have (that spesific worker). their work mail (mail adress) and for last it shall also include which courses they have or have the responsibility for.
 //the work postions they can have are: Professor, Dekan, Janitor, Principal. (Norwegian: Rektor, Dekan, Vaktmester og Lektor). The notes to myself about the task and so has been moved to a seperate file as to not clog this one up.
-const workers = [
+const workersData = [
     {
         firstname: "",
         lastname: "",
         office: "",
         position: "",
-        mail: "",
+        email: "",
         courses: "",
     }
 ]
+//or shall I use this set up hmm?
+const employees = [
+    {
+        firstname: "Ola",
+        lastname: "Berg",
+        office: "C310",
+        email: "ola.berg@hiof.no",
+        postion: "Professor",
+        courses: "Programmering 1, Webutvikling"
+    }
+]
+
+// remember that stilling shall be its own list and that it shall be sued in the indexsen from this list to explain which stilling one employye/worker (ansatt) has in the the overview(oversikt) over the employees/workers(ansatte). 
