7342cf4d93cd36a6b20b0fe6dca8f86cc3189609
PastelThingss <143091459+PastelThingss@users.noreply.github.com>
2025-12-04 21:23:21 +0100

    made the correct base structure for my register set up
    its now ready to be finsihed into a final/complete build with polished finsed code.

--- eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/register.js
+++ eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/register.js
@@ -17,20 +17,178 @@ const workersData = [
 //or shall I use this set up hmm?
 const employees = [
     {
-        firstname: "Ola",
+        firstname: "Ola R1",
         lastname: "Berg",
         office: "C310",
-        email: "ola.berg@hiof.no",
-        postion: positions[0],
-        courses: ["Programmering 1", "Webutvikling"]
+        email: "1ola.berg@hiof.no",
+        postion: positions[1],
+        courses: []
+    },
+
+    {
+        firstname: "Ola d1",
+        lastname: "Berg",
+        office: "C310",
+        email: "2ola.berg@hiof.no",
+        postion: positions[2],
+        courses: []
     },
         {
-        firstname: "Ola",
+        firstname: "Ola d2",
         lastname: "Berg",
         office: "C310",
-        email: "ola.berg@hiof.no",
+        email: "3ola.berg@hiof.no",
+        postion: positions[2],
+        courses: []
+    },
+
+        {
+        firstname: "Ola v1",
+        lastname: "Berg",
+        office: "C310",
+        email: "4ola.berg@hiof.no",
+        postion: positions[3],
+        courses: []
+    },
+    {
+        firstname: "Ola v2",
+        lastname: "Berg",
+        office: "C310",
+        email: "5ola.berg@hiof.no",
+        postion: positions[3],
+        courses: []
+    },
+        {
+        firstname: "Ola v3",
+        lastname: "Berg",
+        office: "C310",
+        email: "6ola.berg@hiof.no",
+        postion: positions[3],
+        courses: []
+    },
+
+        {
+        firstname: "Ola p1",
+        lastname: "Berg",
+        office: "C310",
+        email: "7ola.berg@hiof.no",
         postion: positions[0],
-        courses: ["Programmering 1", "Webutvikling"]
+        courses: ["C1"]
+    },
+    {
+        firstname: "Ola p2",
+        lastname: "Berg",
+        office: "C310",
+        email: "8ola.berg@hiof.no",
+        postion: positions[0],
+        courses: ["C2"]
+    },
+        {
+        firstname: "Ola p3",
+        lastname: "Berg",
+        office: "C310",
+        email: "9ola.berg@hiof.no",
+        postion: positions[0],
+        courses: ["C3"]
+    },
+        {
+        firstname: "Ola p4",
+        lastname: "Berg",
+        office: "C310",
+        email: "10ola.berg@hiof.no",
+        postion: positions[0],
+        courses: ["C4"]
+    },
+    {
+        firstname: "Ola p5",
+        lastname: "Berg",
+        office: "C310",
+        email: "11ola.berg@hiof.no",
+        postion: positions[0],
+        courses: ["C5"]
+    },
+
+        {
+        firstname: "Ola L1",
+        lastname: "Berg",
+        office: "C310",
+        email: "12ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C6"]
+    },
+            {
+        firstname: "Ola L2",
+        lastname: "Berg",
+        office: "C310",
+        email: "13ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C7"]
+    },
+
+        {
+        firstname: "Ola L3",
+        lastname: "Berg",
+        office: "C310",
+        email: "14ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C8"]
+    },
+    {
+        firstname: "Ola L4",
+        lastname: "Berg",
+        office: "C310",
+        email: "15ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C9"]
+    },
+        {
+        firstname: "Ola L5",
+        lastname: "Berg",
+        office: "C310",
+        email: "16ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C10"]
+    },
+
+        {
+        firstname: "Ola L6",
+        lastname: "Berg",
+        office: "C310",
+        email: "17ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C11"]
+    },
+    {
+        firstname: "Ola L7",
+        lastname: "Berg",
+        office: "C310",
+        email: "18ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C12"]
+    },
+        {
+        firstname: "Ola L8",
+        lastname: "Berg",
+        office: "C310",
+        email: "19ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C13","2C3"]
+    },
+        {
+        firstname: "Ola L9",
+        lastname: "Berg",
+        office: "C310",
+        email: "20ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C14","2C2"]
+    },
+    {
+        firstname: "Ola L10",
+        lastname: "Berg",
+        office: "C310",
+        email: "21ola.berg@hiof.no",
+        postion: positions[4],
+        courses: ["C15","2C1"]
     }
 ]
 
